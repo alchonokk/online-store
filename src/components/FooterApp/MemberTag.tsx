696c9e78e53e0e416757ad6827ba7b1d695ba7f6
Zhuk1305 <vaneczhuk@gmail.com>
2022-12-29 16:28:42 +0100

    refactor: #4 - refactor code

--- src/components/FooterApp/MemberTag.tsx
+++ src/components/FooterApp/MemberTag.tsx
@@ -1,12 +1,20 @@
-import { TeamMember } from "../../enumerations/Team";
-import { getMemberGitHubLink } from "../../enumerations/Team";
+interface TeamMember {
+  gitHubProfile: string;
+  name: string;
+}
 
 function MemberTag({ gitHubProfile, name }: TeamMember) {
   return (
-      <a className="git-link" key={gitHubProfile} href={getMemberGitHubLink(gitHubProfile)} target="_blank">
-        <span className="git-logo"></span>
-        <span className="user-name">{name}</span>
-      </a>
+    <a
+      className="git-link"
+      key={gitHubProfile}
+      href={gitHubProfile}
+      target="_blank"
+      rel="noreferrer"
+    >
+      <span className="git-logo"></span>
+      <span className="user-name">{name}</span>
+    </a>
   );
 }
 
