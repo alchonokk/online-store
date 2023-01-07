b684c289504aa5336de691937d12948a8aa9625f
alchonokk <galunia_suglob@mail.ru>
2023-01-07 19:04:13 +0300

    feat: add function for small img

--- src/helpers/createUrlForImage.tsx
+++ src/helpers/createUrlForImage.tsx
@@ -1,6 +1,12 @@
-function createUrlForImage(numericOfImage: string): string {
+export function createUrlForImage(numericOfImage: string): string {
   const urlImage = `https://i.dummyjson.com/data/products/${numericOfImage}/thumbnail.jpg`;
   return urlImage;
 }
 
-export default createUrlForImage;
+export function createUrlForImages(
+  numericOfImage: string,
+  numberImage: string
+): string {
+  const urlImage = `https://i.dummyjson.com/data/products/${numericOfImage}/${numberImage}.jpg`;
+  return urlImage;
+}
