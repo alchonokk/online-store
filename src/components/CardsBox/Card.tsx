47f60c0fd9a7b3a95c92a670679893b57bd65402
alchonokk <galunia_suglob@mail.ru>
2023-01-07 19:02:58 +0300

    fix: separate component for buttonCounterBox

--- src/components/CardsBox/Card.tsx
+++ src/components/CardsBox/Card.tsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import cx from "classnames";
-import createUrlForImage from "../../helpers/createUrlForImage";
-import CounterBox from "../Common/CounterBox";
-import { useAppDispatch } from "../../app/hooks";
-import {
-  addProduct,
-  changeProductAmount,
-  deleteProduct,
-} from "../../reducer/basketReducer/basketslice";
-
+import { createUrlForImage } from "../../helpers/createUrlForImage";
 import Links from "../../constants/Links";
+import { CartButton } from "../Common/ButtonCounterBox";
 
 type CardType = {
   id: string;
@@ -25,47 +16,6 @@ type CardType = {
 };
 
 const Card = ({ id, name, category, price, imgNumber, stock }: CardType) => {
-  const [num, setNum] = useState(0);
-  const [isAddToCard, setIsAddToCard] = useState(false);
-  const dispatch = useAppDispatch();
-
-  const handleIncrement = (count: number) => {
-    if (count < Number(stock)) {
-      setNum(count + 1);
-      dispatch(
-        changeProductAmount({ id, amount: count + 1, price: Number(price) })
-      );
-    } else {
-      setNum(Number(stock));
-      dispatch(
-        changeProductAmount({ id, amount: Number(stock), price: Number(price) })
-      );
-    }
-  };
-
-  const handleDecrement = (count: number) => {
-    count <= 1 ? setNum(0) : setNum(count - 1);
-    dispatch(
-      changeProductAmount({ id, amount: count - 1, price: Number(price) })
-    );
-    if (count <= 1) {
-      setIsAddToCard(!isAddToCard);
-      dispatch(deleteProduct(id));
-    }
-  };
-
-  const addedToCart = (num: number) => {
-    if (!isAddToCard) {
-      setIsAddToCard(true);
-      dispatch(addProduct({ id, amount: num, price: Number(price) }));
-      handleIncrement(num);
-    }
-  };
-
-  const cl = cx("add-to-cart", {
-    "add-to-cart__active": isAddToCard,
-  });
-
   return (
     <div className="card">
       <img src={createUrlForImage(imgNumber)} alt="Card img" />
@@ -76,18 +26,7 @@ const Card = ({ id, name, category, price, imgNumber, stock }: CardType) => {
           More detailed
         </Link>
       </div>
-      <div className={cl} onClick={() => addedToCart(num)}>
-        <div className="add-to-cart-title">
-          {!isAddToCard ? `Add to card: ${price}$` : "Added to card"}
-        </div>
-        {isAddToCard && (
-          <CounterBox
-            num={num}
-            handleDecrement={handleDecrement}
-            handleIncrement={handleIncrement}
-          />
-        )}
-      </div>
+      {<CartButton id={id} price={price} stock={stock} />}
     </div>
   );
 };
