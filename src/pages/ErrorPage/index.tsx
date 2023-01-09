5867fc53e08801efec8f0eddefac65dd0b595675
Zhuk1305 <vaneczhuk@gmail.com>
2023-01-09 12:59:46 +0100

    feat: add style for 404 page

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 import Links from "../../constants/Links";
 
+import "./style.scss";
+
 function ErrorPage() {
   return (
-    <div>
-      <h1>404</h1>
+    <div className="error-page">
+      <h1>{"PAGE NOT FOUND (404)"}</h1>
       <NavLink to={Links.MAIN_PAGE}> Go Main Page! </NavLink>
     </div>
   );
